6cb318f5daa0ca9dfbec5d4b0d5f5eaf66e84933
Ronniy Joseph <ronniy.joseph@icrar.org>
2020-08-11 13:13:14 +1000

    Transitioned to Class based covariance

--- Plot_Residuals_Sky.py
+++ Plot_Residuals_Sky.py
@@ -2,28 +2,33 @@ import numpy
 import argparse
 from matplotlib import colors
 
-from src.covariance import sky_covariance
-from src.covariance import beam_covariance
-from src.powerspectrum import compute_power
-from src.powerspectrum import from_frequency_to_eta
-from src.plottools import plot_2dpower_spectrum
+from src.covariance import SkyCovariance
+from src.covariance import BeamCovariance
+from pyrem.powerspectrum import from_frequency_to_eta
+from pyrem.plottools import plot_2dpower_spectrum
 
 
 def main(labelfontsize = 16, ticksize= 11):
-    k_perp_range = numpy.array([1e-4, 1.1e-1])
+    model_limit = 100e-3
+    broken_fraction = 0.25
     u_range = numpy.logspace(-1, numpy.log10(500), 100)
     frequency_range = numpy.linspace(135, 165, 251) * 1e6
     eta = from_frequency_to_eta(frequency_range)
 
-    position_error_power = calculate_sky_power_spectrum(u=u_range, nu=frequency_range)
-    beam_error_power = calculate_beam_power_spectrum(u=u_range, nu=frequency_range)
-    total_error_power = calculate_total_power_spectrum(u=u_range, nu=frequency_range)
+    sky_covariance = SkyCovariance(model_depth=model_limit)
+    sky_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+    beam_covariance= BeamCovariance(model_depth=model_limit, calibration_type='sky', broken_fraction = broken_fraction)
+    beam_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+
+    sky_error_power = sky_covariance.compute_power()
+    beam_error_power = beam_covariance.compute_power()
+    total_error_power = sky_error_power + beam_error_power
 
     figure, axes = pyplot.subplots(1, 3, figsize=(15, 5))
 
     ps_norm = colors.LogNorm(vmin=1e3, vmax=1e15)
 
-    plot_2dpower_spectrum(u_range, eta, frequency_range, position_error_power, title="Sky Model Error", axes=axes[0],
+    plot_2dpower_spectrum(u_range, eta, frequency_range, sky_error_power, title="Sky Model Error", axes=axes[0],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
                         xlabel_show=True, norm=ps_norm, ylabel_show=True)
 
@@ -31,49 +36,16 @@ def main(labelfontsize = 16, ticksize= 11):
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
                         xlabel_show=True, norm=ps_norm)
 
-    plot_2dpower_spectrum(u_range, eta, frequency_range, total_error_power, title="Total Modelling Error", axes=axes[2],
+    plot_2dpower_spectrum(u_range, eta, frequency_range, total_error_power, title="Total Error", axes=axes[2],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
                         xlabel_show=True, norm=ps_norm, zlabel_show=True)
 
     figure.tight_layout()
-
+    figure.savefig("../plots/Uncalibrated_sky_residuals.pdf")
     pyplot.show()
 
     return
 
-
-def calculate_sky_power_spectrum(u, nu):
-    variance = numpy.zeros((len(u), int(len(nu) / 2)))
-
-    print(f"Calculating covariances for all baselines")
-    for i in range(len(u)):
-        nu_cov = sky_covariance(u[i], v=0, nu=nu)
-        variance[i, :] = compute_power(nu, nu_cov)
-
-    return variance
-
-
-def calculate_beam_power_spectrum(u, nu):
-    variance = numpy.zeros((len(u), int(len(nu) / 2)))
-
-    print(f"Calculating covariances for all baselines")
-    for i in range(len(u)):
-        nu_cov = beam_covariance(u[i], v=0, nu=nu, calibration_type='sky')
-        variance[i, :] = compute_power(nu, nu_cov)
-
-    return variance
-
-
-def calculate_total_power_spectrum(u, nu):
-    variance = numpy.zeros((len(u), int(len(nu) / 2)))
-
-    print(f"Calculating covariances for all baselines")
-    for i in range(len(u)):
-        nu_cov = beam_covariance(u= u[i], v=0, nu=nu, calibration_type="sky") + \
-                 sky_covariance(u=u[i], v=0, nu=nu)
-        variance[i, :] = compute_power(nu, nu_cov)
-
-    return variance
 
 
 if __name__ == "__main__":
