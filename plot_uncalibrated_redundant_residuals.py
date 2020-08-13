0639953c094329277781f1185213883d9adbed08
Ronniy Joseph <ronniy.joseph@icrar.org>
2020-08-13 17:23:57 +1000

    Changed to class based calculations

--- plot_uncalibrated_redundant_residuals.py
+++ plot_uncalibrated_redundant_residuals.py
@@ -2,22 +2,28 @@ import numpy
 import argparse
 from matplotlib import colors
 
-from src.covariance import position_covariance
-from src.covariance import beam_covariance
-from src.powerspectrum import compute_power
-from src.powerspectrum import from_frequency_to_eta
-from src.plottools import plot_2dpower_spectrum
+from src.covariance import PositionCovariance
+from src.covariance import BeamCovariance
+from pyrem.powerspectrum import from_frequency_to_eta
+from pyrem.plottools import plot_2dpower_spectrum
 
 
 def main(labelfontsize = 20, ticksize= 15):
+    tile_precision = 1e-2
+    broken_fraction = 0.25
     k_perp_range = numpy.array([1e-4, 1.1e-1])
     u_range = numpy.logspace(-1, numpy.log10(500), 100)
     frequency_range = numpy.linspace(135, 165, 251) * 1e6
     eta = from_frequency_to_eta(frequency_range)
 
-    position_error_power = calculate_position_power_spectrum(u=u_range, nu=frequency_range)
-    beam_error_power = calculate_beam_power_spectrum(u=u_range, nu=frequency_range)
-    total_error_power = calculate_total_power_spectrum(u=u_range, nu=frequency_range)
+    position_covariance = PositionCovariance(position_precision=tile_precision)
+    position_covariance.compute_covariance(u=u_range, v=0, nu=frequency_range)
+    beam_covariance = BeamCovariance(model_depth=10, calibration_type='redundant', broken_fraction=broken_fraction)
+    beam_covariance.compute_covariance(u=u_range, v=0, nu = frequency_range)
+
+    position_error_power = position_covariance.compute_power()
+    beam_error_power = beam_covariance.compute_power()
+    total_error_power = position_error_power + beam_error_power
 
     figure, axes = pyplot.subplots(1, 3, figsize=(15, 5))
 
@@ -27,53 +33,20 @@ def main(labelfontsize = 20, ticksize= 15):
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
                         xlabel_show=True, norm=ps_norm, ylabel_show=True)
 
-    plot_2dpower_spectrum(u_range, eta, frequency_range, beam_error_power, title="Beam Variations", axes=axes[1],
+    plot_2dpower_spectrum(u_range, eta, frequency_range, beam_error_power, title="Beam Model Error", axes=axes[1],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
                         xlabel_show=True, norm=ps_norm)
 
-    plot_2dpower_spectrum(u_range, eta, frequency_range, total_error_power, title="Total Noise", axes=axes[2],
+    plot_2dpower_spectrum(u_range, eta, frequency_range, total_error_power, title="Total Error", axes=axes[2],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
                         xlabel_show=True, norm=ps_norm, zlabel_show=True)
 
     figure.tight_layout()
+    figure.savefig("../plots/Uncalibrated_redundant_residuals.pdf")
 
-    pyplot.show()
 
     return
 
-
-def calculate_position_power_spectrum(u, nu):
-    variance = numpy.zeros((len(u), int(len(nu) / 2)))
-
-    print(f"Calculating covariances for all baselines")
-    for i in range(len(u)):
-        nu_cov = position_covariance(u[i], v=0, nu=nu)
-        variance[i, :] = compute_power(nu, nu_cov)
-
-    return variance
-
-
-def calculate_beam_power_spectrum(u, nu):
-    variance = numpy.zeros((len(u), int(len(nu) / 2)))
-
-    print(f"Calculating covariances for all baselines")
-    for i in range(len(u)):
-        nu_cov = beam_covariance(u[i], v=0, nu=nu, calibration_type="redundant")
-        variance[i, :] = compute_power(nu, nu_cov)
-
-    return variance
-
-
-def calculate_total_power_spectrum(u, nu):
-    variance = numpy.zeros((len(u), int(len(nu) / 2)))
-
-    print(f"Calculating covariances for all baselines")
-    for i in range(len(u)):
-        nu_cov = beam_covariance(u= u[i], v=0, nu=nu, calibration_type="redundant") + \
-                 position_covariance(u=u[i], v=0, nu=nu)
-        variance[i, :] = compute_power(nu, nu_cov)
-
-    return variance
 
 
 if __name__ == "__main__":
