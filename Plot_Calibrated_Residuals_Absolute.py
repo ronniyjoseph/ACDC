9434b2aecc1bb8b27062922a6cbfd5045070e397
Ronniy Joseph <ronniy.joseph@icrar.org>
2020-08-17 15:23:38 +1000

    Moved to class based

--- Plot_Calibrated_Residuals_Absolute.py
+++ Plot_Calibrated_Residuals_Absolute.py
@@ -1,47 +1,82 @@
 import numpy
 import argparse
+import copy
+import time
 from matplotlib import colors
 
-from src.powerspectrum import from_frequency_to_eta
+from src.covariance import SkyCovariance
+from src.covariance import BeamCovariance
+from src.covariance import GainCovariance
+from src.covariance import CalibratedResiduals
+from src.covariance import compute_weights
+from pyrem.skymodel import sky_moment_returner
+from pyrem.powerspectrum import from_frequency_to_eta
+from pyrem.plottools import plot_1dpower_spectrum
+
+from pyrem.powerspectrum import from_frequency_to_eta
 from src.powerspectrum import fiducial_eor_power_spectrum
 
-from src.plottools import plot_1dpower_spectrum
-# from src.plottools import plot_1dpower_contours
-from src.generaltools import from_jansky_to_milikelvin
+from pyrem.radiotelescope import RadioTelescope
 
-from src.covariance import calibrated_residual_error
+from pyrem.plottools import plot_2dpower_spectrum
+from pyrem.plottools import plot_power_contours
+from pyrem.generaltools import from_jansky_to_milikelvin
+
+from pyrem.covariance import calibrated_residual_error
+
+from pyrem.util import redundant_baseline_finder
 
 
 def main(labelfontsize = 20, ticksize= 15):
-    output_path = "./" #"/data/rjoseph/Hybrid_Calibration/theoretical_calculations/power_spectra/"
-    k_perp_range = numpy.array([1e-4, 1.1e-1])
-    u_range = numpy.logspace(-1, numpy.log10(500), 100)
+    model_limit = 100e-3
+    broken_fraction = 0.25
     u_plot = 7
-    model_limit = 1e-1
-    broken_baseline_weight = 0.3
-    frequency_range = numpy.linspace(135, 165, 251) * 1e6
-    contour_levels = numpy.array([1e1, 1e2, 1e3])
 
+    telescope_position_path = "./data/MWA_Compact_Coordinates.txt"
 
-
+    u_range = numpy.logspace(-1, numpy.log10(500), 100)
+    frequency_range = numpy.linspace(135, 165,251 )* 1e6
     eta = from_frequency_to_eta(frequency_range)
+
     eor_power_spectrum = fiducial_eor_power_spectrum(u_range, eta)
 
-    sky_calibrated = calibrated_residual_error(u=u_range, nu=frequency_range, residuals='sky',
-                                                                 calibration_type='absolute', model_limit=model_limit,
-                                               broken_baselines_weight=broken_baseline_weight)
-    beam_calibrated = calibrated_residual_error(u=u_range, nu=frequency_range, residuals='beam',
-                                                         calibration_type='absolute', model_limit=model_limit,
-                                               broken_baselines_weight=broken_baseline_weight)
-    total_calibrated = calibrated_residual_error(u=u_range, nu=frequency_range, residuals='both',
-                                                           calibration_type='absolute', model_limit=model_limit,
-                                               broken_baselines_weight=broken_baseline_weight)
+    telescope = RadioTelescope(load=True, path=telescope_position_path)
+    redundant_table = redundant_baseline_finder(telescope.baseline_table)
+
+    sky_covariance = SkyCovariance(model_depth=model_limit)
+    sky_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+
+    model_sky = copy.deepcopy(sky_covariance)
+    unmodeled_mu = sky_moment_returner(2, s_low=sky_covariance.s_low, s_mid=sky_covariance.s_mid,
+                                       s_high=sky_covariance.model_depth, k1=sky_covariance.k1,
+                                       gamma1=sky_covariance.alpha1, k2=sky_covariance.k2, gamma2=sky_covariance.alpha2)
+    modeled_mu = sky_moment_returner(2, s_low=sky_covariance.model_depth, s_mid=sky_covariance.s_mid,
+                                       s_high=sky_covariance.s_high, k1=sky_covariance.k1,
+                                       gamma1=sky_covariance.alpha1, k2=sky_covariance.k2, gamma2=sky_covariance.alpha2)
+
+    model_sky.matrix *= modeled_mu / unmodeled_mu
+
+    beam_covariance = BeamCovariance(model_depth=model_limit, calibration_type='sky', broken_fraction=broken_fraction)
+    beam_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+    total_covariance = sky_covariance + beam_covariance
+
+    sky_gain = GainCovariance(sky_covariance, calibration_type='absolute', baseline_table=redundant_table)
+    beam_gain = GainCovariance(beam_covariance, calibration_type='absolute', baseline_table=redundant_table)
+    total_gain= GainCovariance(total_covariance, calibration_type='absolute', baseline_table=redundant_table)
+
+    sky_residuals = CalibratedResiduals(sky_gain, model_matrix=model_sky, residual_matrix=sky_covariance)
+    beam_residuals = CalibratedResiduals(beam_gain, model_matrix=model_sky, residual_matrix=sky_covariance)
+    total_residuals = CalibratedResiduals(total_gain, model_matrix=model_sky, residual_matrix=sky_covariance)
+
+    sky_power = sky_residuals.compute_power()
+    beam_power = beam_residuals.compute_power()
+    total_power = total_residuals.compute_power()
 
     index = numpy.where(numpy.abs(u_range - u_plot) == numpy.min(numpy.abs(u_range - u_plot)))[0]
 
-    sky_calibrated = sky_calibrated[index, ...]
-    beam_calibrated= beam_calibrated[index,...]
-    total_calibrated= total_calibrated[index, ...]
+    sky_calibrated = sky_power[index, ...]
+    beam_calibrated= beam_power[index,...]
+    total_calibrated= total_power[index, ...]
     eor_power_spectrum = eor_power_spectrum[index,...]
 
     figure, axes = pyplot.subplots(1, 3, figsize=(15, 5))
@@ -71,7 +106,7 @@ def main(labelfontsize = 20, ticksize= 15):
     # plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(total_calibrated, frequency_range)/eor_power_spectrum,
     #                     axes=axes[2], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True, norm=ps_norm, ylabel_show=False, contour_levels=contour_levels)
     pyplot.tight_layout()
-    pyplot.savefig(output_path + "Calibrated_Residuals_Absolute.pdf")
+    pyplot.savefig("../plots/Calibrated_Residuals_Absolute_MWA.pdf")
     pyplot.show()
     return
 
