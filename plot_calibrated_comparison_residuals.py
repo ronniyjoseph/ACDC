577cbb9ffaa6f2572fceaf807c62a54bbbbfdeab
Ronniy Joseph <ronniy.joseph@icrar.org>
2020-08-20 13:49:21 +1000

    Removed unneccesary code

--- plot_calibrated_comparison_residuals.py
+++ plot_calibrated_comparison_residuals.py
@@ -22,54 +22,62 @@ from src.powerspectrum import fiducial_eor_power_spectrum
 from pyrem.radiotelescope import RadioTelescope
 from pyrem.generaltools import from_jansky_to_milikelvin
 
-def main(labelfontsize = 20, ticksize= 15):
+def main(labelfontsize = 20, ticksize= 20):
     model_limit = 100e-3
     position_error = 0.01
     broken_fraction = 0.25
+    compute = False
+    save = False
+    load = True
     telescope_position_path = "./data/MWA_Compact_Coordinates.txt"
 
     u_range = numpy.logspace(-1, numpy.log10(500), 100)
     frequency_range = numpy.linspace(135, 165, 251 )* 1e6
     eta = from_frequency_to_eta(frequency_range)
-
-    # eor_power_spectrum = fiducial_eor_power_spectrum(u_range, eta)
+    print(f"{eta.max()} blaah")
+    eor_power_spectrum = fiducial_eor_power_spectrum(u_range, eta)
 
     telescope = RadioTelescope(load=True, path=telescope_position_path)
     redundant_table = redundant_baseline_finder(telescope.baseline_table)
 
-    contour_levels = numpy.array([1e0, 1e1, 1e2])
-    sky_clocations = [(6e-2, 0.21), (4e-2, 0.17), (3e-2, 0.07 )]
-    beam_clocations = [(6e-2, 0.21), (0.045, 0.15), (3e-2, 0.07 )]
-    total_clocations = [(6e-2, 0.24), (0.045, 0.18), (3e-2, 0.10)]
+    contour_levels = numpy.array([1e-1, 1e0, 1e2])
+    sky_clocations = None#[(6e-2, 0.21), (4e-2, 0.25), (1.1e-2, 0.35 )]
+    beam_clocations = None #[(6e-2, 0.21), (8e-2, 0.5), (1.1e-2, 0.35 )]
+    linestyles = ['dotted','dashed','solid' ]
 
 
+    if compute:
     #### Initialise Sky Errors ###
-    # sky_error = SkyCovariance(model_depth=model_limit)
-    # sky_error.compute_covariance(u=u_range, v = 0, nu=frequency_range)
-    # sky_beam_covariance = BeamCovariance(model_depth=model_limit, calibration_type='sky', broken_fraction=broken_fraction)
-    # sky_beam_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
-    # sky_total = sky_error + sky_beam_covariance
+        sky_error = SkyCovariance(model_depth=model_limit)
+        sky_error.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+        sky_beam_covariance = BeamCovariance(model_depth=model_limit, calibration_type='sky', broken_fraction=broken_fraction)
+        sky_beam_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+        sky_total = sky_error + sky_beam_covariance
 
-    with open('unmodeled_sky.obj', 'rb') as f:
-        sky_error = pickle.load(f)
-    with open('sky_total.obj','rb') as f:
-        sky_total = pickle.load(f)
+    if load:
+        with open('unmodeled_sky.obj', 'rb') as f:
+            sky_error = pickle.load(f)
+        with open('sky_total.obj','rb') as f:
+            sky_total = pickle.load(f)
 
     sky_gain = GainCovariance(sky_total, calibration_type='sky', baseline_table=redundant_table)
 
-
-    ###### Initialise Redundant Errors
-    # position_covariance = PositionCovariance(position_precision=position_error)
-    # position_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
-    # beam_covariance = BeamCovariance(model_depth=model_limit, calibration_type='relative', broken_fraction=broken_fraction)
-    # beam_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
-    # redundant_total = position_covariance + beam_covariance
-    with open('redundant_residuals.obj','rb') as f:
-        redundant_total = pickle.load(f)
+    if compute:
+        ###### Initialise Redundant Errors
+        position_covariance = PositionCovariance(position_precision=position_error)
+        position_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+        beam_covariance = BeamCovariance(model_depth=model_limit, calibration_type='relative', broken_fraction=broken_fraction)
+        beam_covariance.compute_covariance(u=u_range, v = 0, nu=frequency_range)
+        redundant_total = position_covariance + beam_covariance
+    if load:
+        with open('redundant_residuals.obj','rb') as f:
+           redundant_total = pickle.load(f)
 
     relative_gain= GainCovariance(redundant_total, calibration_type='relative', baseline_table=redundant_table)
     absolute_gain= GainCovariance(sky_total, calibration_type='absolute', baseline_table=redundant_table)
     redundant_gain = relative_gain + absolute_gain
+
+
     ##### Rescale unmodeled sky covariance to model (avoiding heavy computation)
     model_sky = residual_to_model_rescale(sky_error)
 
@@ -81,16 +89,17 @@ def main(labelfontsize = 20, ticksize= 15):
     redundant_power = redundant_residuals.compute_power()
     sky_power = sky_residuals.compute_power()
 
+    if save:
     ###### Save covariance matrices
-    # sky_error.save("unmodeled_sky")
-    # sky_beam_covariance.save("sky_beam_error")
-    # sky_total.save("sky_total")
-    #
-    # position_covariance.save("position_error")
-    # beam_covariance.save("redundant_beam")
-    # redundant_total.save("redundant_residuals")
+        sky_error.save("unmodeled_sky")
+        sky_beam_covariance.save("sky_beam_error")
+        sky_total.save("sky_total")
 
-    figure, axes = pyplot.subplots(1, 3, figsize=(15, 5))
+        position_covariance.save("position_error")
+        beam_covariance.save("redundant_beam")
+        redundant_total.save("redundant_residuals")
+
+    figure, axes = pyplot.subplots(1, 2, figsize=(11, 5))
     ps_norm = colors.LogNorm(vmin=1e3, vmax=1e15)
 
     plot_2dpower_spectrum(u_range, eta, frequency_range, sky_power, title="Sky Based", axes=axes[0],
@@ -98,25 +107,21 @@ def main(labelfontsize = 20, ticksize= 15):
                         xlabel_show=True, norm=ps_norm, ylabel_show=True)
 
     plot_2dpower_spectrum(u_range, eta, frequency_range, redundant_power, title="Redundancy Based", axes=axes[1],
-                        axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=False,
+                        axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True, zlabel_show=True,
                         xlabel_show=True, norm=ps_norm, ylabel_show=False)
 
-    # plot_2dpower_spectrum(u_range, eta, frequency_range, total_power, title="Total Error", axes=axes[2],
-    #                     axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
-    #                     xlabel_show=True, norm=ps_norm, zlabel_show=True, ylabel_show=False)
 
-    # plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(sky_power, frequency_range)/eor_power_spectrum,
-    #                     axes=axes[0], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True,
-    #                     norm=ps_norm, ylabel_show=False, contour_levels=contour_levels, contour_label_locs=sky_clocations)
-    #
-    # plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(redundant_power, frequency_range)/eor_power_spectrum,
-    #                     axes=axes[1], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True,
-    #                     norm=ps_norm, ylabel_show=False, contour_levels=contour_levels, contour_label_locs=beam_clocations)
+    plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(sky_power, frequency_range)/eor_power_spectrum,
+                        axes=axes[0], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True,
+                        norm=ps_norm, ylabel_show=False, contour_levels=contour_levels, contour_label_locs=sky_clocations,
+                        smooth = 3, contour_styles = linestyles)
 
-    # plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(total_calibrated, frequency_range)/eor_power_spectrum,
-    #                     axes=axes[2], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True,
-    #                     norm=ps_norm, ylabel_show=False, contour_levels=contour_levels, contour_label_locs=total_clocations)
-    #
+    plot_power_contours(u_range, eta, frequency_range, from_jansky_to_milikelvin(redundant_power, frequency_range)/eor_power_spectrum,
+                        axes=axes[1], ratio=True, axes_label_font=labelfontsize, tickfontsize=ticksize, xlabel_show=True,
+                        norm=ps_norm, ylabel_show=False, contour_levels=contour_levels, contour_label_locs=beam_clocations,
+                        smooth =3, contour_styles = linestyles)
+
+
 
     pyplot.tight_layout()
     pyplot.savefig("../plots/Calibrated_Residuals_Comparison_MWA.pdf")
